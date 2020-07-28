20644ecced3556b12a6d38fd762a45136e6118de
martucazpo <martucazpo@gmail.com>
2020-07-28 08:58:39 -0700

    database connected

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
-  }
+}
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const PORT = process.env.PORT;
 
 app.get('/', (req, res) => {
     res.send("Hello");
-})
+});
+
+mongoose.connect(process.env.DATABASE_URL || process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true
+    },
+    () => {
+        console.log("My database is connected");
+    }
+);
 
 app.listen(PORT, () => {
     console.log("Tiny electronic ears are listening on port " + PORT);
