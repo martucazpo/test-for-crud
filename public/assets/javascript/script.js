391c8c2455919b704786ab662044da28591e5920
martucazpo <martucazpo@gmail.com>
2020-07-28 18:08:18 -0700

    ejs variables to js

--- public/assets/javascript/script.js
+++ public/assets/javascript/script.js
@@ -1,1 +1,7 @@
-console.log("I'm here");
+document.addEventListener("DOMContentLoaded", function () {
+    const catTitle = document.getElementById('catTitle');
+    const catAge = document.getElementById('catAge');
+    let name = catTitle.getAttribute('data-name');
+    let age = catAge.getAttribute('data-age');
+    console.log(name, age);
+});
